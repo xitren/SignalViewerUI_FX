29a92d0f45dae955d6dea977bd731ff9e8356117
xitren <xitren@ya.ru>
2021-02-02 01:16:15 +0300

    Not intended

--- src/main/java/com/gusev/fx/signal_ui/SignalUIController.java
+++ src/main/java/com/gusev/fx/signal_ui/SignalUIController.java
@@ -25,7 +25,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class SignalUIController implements Initializable {
@@ -73,12 +72,6 @@ public class SignalUIController implements Initializable {
     private GraphController graphCtrl = null;
     private MarksController marksCtrl = null;
     private ResourceBundle resources;
-
-    private Optional<String> getExtensionByStringHandling(String filename) {
-        return Optional.ofNullable(filename)
-                .filter(f -> f.contains("."))
-                .map(f -> f.substring(filename.lastIndexOf(".") + 1));
-    }
 
     public void setStage(Stage stage) {
         this.stage = stage;
@@ -200,29 +193,20 @@ public class SignalUIController implements Initializable {
         datafx.clearMarks();
     }
 
-
     public void OnSaveFile(ActionEvent actionEvent) {
         FileChooser fileChooser = new FileChooser();
         //Set extension filter for text files
-        FileChooser.ExtensionFilter extJSONFilter =
-                new FileChooser.ExtensionFilter("JSON data files (*.json)", "*.json");
-        FileChooser.ExtensionFilter extTXTFilter =
-                new FileChooser.ExtensionFilter("TXT data files (*.txt)", "*.txt");
-        fileChooser.getExtensionFilters().addAll(extJSONFilter, extTXTFilter);
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("JSON data files (*.json)", "*.json");
+        fileChooser.getExtensionFilters().add(extFilter);
         fileChooser.setInitialDirectory(new File("./"));
         //Show save file dialog
         File file = fileChooser.showSaveDialog(stage);
-        Optional<String> ext = getExtensionByStringHandling(file.getName());
-        try {
-            if (file != null && ext.isPresent()) {
-                if (ext.get().equals("txt")) {
-                    this.datafx.saveToTXT(file.getAbsolutePath());
-                } else {
-                    this.datafx.saveToFile(file.getAbsolutePath());
-                }
+        if (file != null) {
+            try {
+                this.datafx.saveToFile(file.getAbsolutePath());
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 
